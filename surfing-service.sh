7592e200ed31d25e0f7b6d39e29e677fc0a385aa
vsenger <vinicius@globalcode.com.br>
2016-04-25 12:18:50 -0300

    fix linux script

--- surfing-service.sh
+++ surfing-service.sh
@@ -1,4 +1,7 @@
+java -classpath "./target/iot-surfing-service-1.0-SNAPSHOT.jar:./target/dependency/*" -Dsurfing.config="etc/config"  org.surfing.kernel.Kernel $1
+#Use this to use a custom JDK
+#export JAVA_HOME=/jdk1.8.0_60
+#$JAVA_HOME/bin/java -classpath "target\iot-surfing-service-1.0-SNAPSHOT.jar:target\dependency\*" -Dsurfing.config="etc/config"  org.surfing.kernel.Kernel %1
 
-export JAVA_HOME=/jdk1.8.0_60
 
-$JAVA_HOME/bin/java -classpath "target\iot-surfing-service-1.0-SNAPSHOT.jar;target\dependency\*" -Dsurfing.config="etc/config"  org.surfing.kernel.Kernel %1
+
